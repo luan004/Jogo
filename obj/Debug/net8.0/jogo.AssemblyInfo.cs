0da3750ab3983efd404103334f493826fb2ac940
Luan Gabriel <38366327+luan004@users.noreply.github.com>
2024-12-31 20:37:41 -0300

    .

--- obj/Debug/net8.0/jogo.AssemblyInfo.cs
+++ obj/Debug/net8.0/jogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("jogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70e23ce973fb33013cc630ea838e8a07dd24b158")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b33e752b7a834e540566130f4d606d1d085f0c3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("jogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("jogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
